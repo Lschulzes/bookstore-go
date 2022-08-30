7f69d7e35a130518f0ec3c602103c3e411cbb433
lschulzes <lucassilvaiee@gmail.com>
2022-08-29 21:01:37 -0300

    feat: add base Book crud ops

--- pkg/models/book.go
+++ pkg/models/book.go
@@ -22,3 +22,27 @@ func init() {
 		return
 	}
 }
+
+func (b *Book) CreateBook() *Book {
+	db.Create(&b)
+	return b
+}
+
+func GetAllBooks() []Book {
+	var books []Book
+	db.Find(&books)
+	return books
+}
+
+func GetBook(id int64) (*Book, *gorm.DB) {
+	var book Book
+	db := db.Where("ID=?", id).Find(&book)
+
+	return &book, db
+}
+
+func DeleteBook(id int64) Book {
+	var book Book
+	db.Where("ID=?", id).Delete(book)
+	return book
+}
